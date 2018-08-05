747c54dd84ff5772b7b72e97ae2b9fcc93531686
Chandan CH <chandanch@users.noreply.github.com>
2018-08-05 13:21:29 +0530

    accessing property values

---
+++ accessing-property-values.js
@@ -0,0 +1,20 @@
+// creation of object and adding properties & values: Generic way of object creation
+var userDetails = {
+  name: 'chandio',
+  zipcode: 3490,
+  age: 22,
+  isActive: true,
+  boiush: "cladameir",
+  preferredBeverage: "Coca shake"
+}
+
+// Method 1: Accessing property value using Dot notation
+console.log("User name", userDetails.name);
+
+// Method 2: Accessing property value using square bracker notation
+console.log("User age", userDetails["age"]);
+console.log("User boiush", userDetails["boiush"]);
+
+// Accessing property value dynamically based on value of the variable
+var code = "zipcode";
+console.log("User zip code", userDetails[code]);
