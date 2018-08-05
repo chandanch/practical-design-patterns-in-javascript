79e7605171f8de6bbe613cfd04edf6b39427a92e
Chandan CH <chandanch@users.noreply.github.com>
2018-08-05 12:46:49 +0530

    object creation

---
+++ object-creation.js
@@ -0,0 +1,17 @@
+/**
+ * Object Creation:
+ * Objects in JS can be created in 3 ways:
+ * 1. Empty curly braces {}
+ * 2. Using the create method: create()
+ * 3. Using the `new` keyword: new Object()
+ */
+
+// Method 1: Using empty curly braces {}
+var player = {}
+
+// Method 2: Using create method
+var task = Object.create(Object.prototype);
+console.log(userDetails);
+// Method 3: Using the new keyword
+var userDetails = new Object();
+console.log(userDetails);
