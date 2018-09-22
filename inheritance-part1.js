842f84ec520e53bf62a34c27324a19dbeb1bd9bc
Chandan CH <chandanch@users.noreply.github.com>
2018-09-22 18:46:36 +0530

    inheritance preview :bulb:

--- inheritance-part1.js
+++ inheritance-part1.js
@@ -2,7 +2,6 @@
 * Inheritance: An object's properties & methods can be inherited   to other objects usingthe Object.createMethod() for inheritance. 
 */
 
-
 // base object
 var task = {};
 task.title = 'Check Log';
