88fc9e0da667aaffbf171225519a74722054d805
chandanch <chandan.cec107@gmail.com>
2018-12-23 14:20:12 +0530

    example for module pattern

---
+++ module/module-pattern.js
@@ -0,0 +1,34 @@
+/**
+* Module Pattern: Provides a way to group like methods together
+* Allows to encapsulate methods. 
+* Create a 'toolbox` with methods that are similar 
+*/
+
+// Example for module patterns: Get Data via Ajax request
+
+var HttpService = function() {
+    // private var
+    var url = 'http://someendpoint';
+
+    var getData = function() {
+        console.log('Fetching from', url);
+        console.log("Data recevied from request");
+    }
+
+    var postData = function(data) {
+        console.log('posting data to..', url);
+        // adding delay to simulate the request
+        setTimeout(function () {
+            console.log('data posted successfully');
+        }, 2000);
+    }
+
+    // return the object with two methods
+    // follows the notion of Revealing module pattern where we list the method names
+    // in the return method
+    return {
+        get: getData,
+        post: postData
+    }
+}
+ 
